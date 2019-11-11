cc2bb746c52dc79d9482a3a916e5d669461c832e
Valerie Foster <fosterv2@outlook.com>
2019-11-11 07:43:14 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,17 @@ const codes = [
 
 function init() {
   // your code here
+  let ind = 0
+  function onKeyDownHandler(e) {
+    const key = e.key
+    if (key === codes[ind]) {
+      ind++;
+      if (ind === codes.length) {
+        alert("Congratulations!");
+        ind = 0;
+      }
+    } else {
+      ind = 0;
+    }
+  }
 }
